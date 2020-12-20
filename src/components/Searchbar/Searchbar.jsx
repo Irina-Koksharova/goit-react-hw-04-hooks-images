a47598d3ed516698ae08e943ea3451db4dfba488
Irina-Koksharova <biodgerelo@ukr.net>
2020-12-20 17:36:11 +0200

    Компонент Searchbar переделан на хуки

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,65 +1,52 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { IconContext } from 'react-icons';
 import s from './Searchbar.module.css';
 
-class SearchBar extends Component {
-  state = {
-    searchQuery: '',
+const SearchBar = ({ onSubmit, onError }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChangeForm = e => {
+    setSearchQuery(e.target.value);
   };
 
-  handleChangeForm = e => {
-    this.setState({ searchQuery: e.currentTarget.value });
-  };
-
-  handleSubmitForm = e => {
-    const { searchQuery } = this.state;
-    const { onError, onSubmit } = this.props;
+  const handleSubmitForm = e => {
+    e.preventDefault();
     const clientError =
       'Incorrect query! Please enter your request in the correct form';
-
-    e.preventDefault();
     if (searchQuery.trim() === '') {
       onError(clientError);
     }
     onSubmit(searchQuery);
-    this.resetForm();
+    setSearchQuery('');
   };
 
-  resetForm = () => {
-    this.setState({
-      searchQuery: '',
-    });
-  };
+  return (
+    <header className={s.container}>
+      <form className={s.form} onSubmit={handleSubmitForm}>
+        <button className={s.button} type="submit">
+          <IconContext.Provider value={{ className: `${s.reactIcons}` }}>
+            <FiSearch />
+          </IconContext.Provider>
+        </button>
 
-  render() {
-    return (
-      <header className={s.container}>
-        <form className={s.form} onSubmit={this.handleSubmitForm}>
-          <button className={s.button} type="submit">
-            <IconContext.Provider value={{ className: `${s.reactIcons}` }}>
-              <FiSearch />
-            </IconContext.Provider>
-          </button>
-
-          <label className={s.label}>
-            <input
-              className={s.input}
-              type="text"
-              name="searchQuery"
-              value={this.state.searchQuery}
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={this.handleChangeForm}
-            />
-          </label>
-        </form>
-      </header>
-    );
-  }
-}
+        <label className={s.label}>
+          <input
+            className={s.input}
+            type="text"
+            name="searchQuery"
+            value={searchQuery}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={handleChangeForm}
+          />
+        </label>
+      </form>
+    </header>
+  );
+};
 
 SearchBar.propTypes = {
   onError: PropTypes.func.isRequired,
