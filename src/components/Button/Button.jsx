1efa1d7d5dc5d3e2d3a602decae98a88f0877c80
Irina-Koksharova <biodgerelo@ukr.net>
2020-12-20 17:21:26 +0200

    Компонент Button переделан на хуки

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,23 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import s from './Button.module.css';
 
-class Button extends Component {
-  loading = () => {
-    const { onClick, page } = this.props;
-    onClick(page);
-  };
-
-  render() {
-    return (
-      <button className={s.button} type="button" onClick={this.loading}>
-        Load more
-      </button>
-    );
-  }
-}
+const Button = ({ page, onClick }) => {
+  return (
+    <button
+      className={s.button}
+      type="button"
+      onClick={() => {
+        onClick(page);
+      }}
+    >
+      Load more
+    </button>
+  );
+};
 
 Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
   page: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
